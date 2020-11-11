9f4c778656c26b76c48f7dbdbb3ad021569c1099
codegangsk <kjhj0604@gmail.com>
2020-11-11 13:36:41 +0900

    define PhotoInfoController class and add fetchPhotoInfo method to it

--- 5.6SpacePhoto/PhotoInfoController.swift
+++ 5.6SpacePhoto/PhotoInfoController.swift
@@ -7,24 +7,26 @@
 
 import Foundation
 
-func fetchPhotoInfo(completion: @escaping (PhotoInfo?) -> Void) {
-    let baseURL = URL(string: "http://api.nasa.gov/planetary/apod")!
+struct PhotoInfoController {
+    func fetchPhotoInfo(completion: @escaping (PhotoInfo?) -> Void) {
+        let baseURL = URL(string: "http://api.nasa.gov/planetary/apod")!
     
-    let query: [String: String] = [
+        let query: [String: String] = [
         "api_key": "DEMO_KEY",
-    ]
+        ]
     
-    let url = baseURL.withQueries(query)!
-    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-        let jsonDecoder = JSONDecoder()
-             if let data = data,
-                let photoInfo = try?
-                    jsonDecoder.decode(PhotoInfo.self, from: data) {
-                completion(photoInfo)
-             } else {
-                print("Either no data was returned, or data was not serialized")
-                completion(nil)
-             }
+        let url = baseURL.withQueries(query)!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            let jsonDecoder = JSONDecoder()
+                if let data = data,
+                   let photoInfo = try?
+                        jsonDecoder.decode(PhotoInfo.self, from: data) {
+                    completion(photoInfo)
+                } else {
+                    print("Either no data was returned, or data was not serialized")
+                    completion(nil)
+                }
+        }
+        task.resume()
     }
-    task.resume()
 }
