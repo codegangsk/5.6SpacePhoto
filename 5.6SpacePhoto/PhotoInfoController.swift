36643cb1169b8b4973e7ccc5d237e1c01c468667
codegangsk <kjhj0604@gmail.com>
2020-11-11 13:25:33 +0900

    declare function that brings JSON data and decode it to custom struct

---
+++ 5.6SpacePhoto/PhotoInfoController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoInfoController.swift
+//  5.6SpacePhoto
+//
+//  Created by Sophie Kim on 2020/11/11.
+//
+
+import Foundation
+
+func fetchPhotoInfo(completion: @escaping (PhotoInfo?) -> Void) {
+    let baseURL = URL(string: "http://api.nasa.gov/planetary/apod")!
+    
+    let query: [String: String] = [
+        "api_key": "DEMO_KEY",
+    ]
+    
+    let url = baseURL.withQueries(query)!
+    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        let jsonDecoder = JSONDecoder()
+             if let data = data,
+                let photoInfo = try?
+                    jsonDecoder.decode(PhotoInfo.self, from: data) {
+                completion(photoInfo)
+             } else {
+                print("Either no data was returned, or data was not serialized")
+                completion(nil)
+             }
+    }
+    task.resume()
+}
