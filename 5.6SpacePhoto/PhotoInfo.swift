0809d107f5845499c74c91de461672c10befbc22
codegangsk <kjhj0604@gmail.com>
2020-11-11 11:34:41 +0900

    create PhotoInfo struct to store decoded data

---
+++ 5.6SpacePhoto/PhotoInfo.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoInfo.swift
+//  5.6SpacePhoto
+//
+//  Created by Sophie Kim on 2020/11/11.
+//
+
+import Foundation
+
+struct PhotoInfo: Codable {
+    var title: String
+    var description: String
+    var url: URL
+    var copyright: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description
+        case url
+        case copyright
+    }
+    
+    init(from decoder: Decoder) throws {
+        let valueContainer = try decoder.container(keyedBy: CodingKeys.self)
+        self.title = try valueContainer.decode(String.self, forKey: CodingKeys.title)
+        self.description = try valueContainer.decode(String.self, forKey: CodingKeys.description)
+        self.url = try valueContainer.decode(URL.self, forKey: CodingKeys.url)
+        self.copyright = try? valueContainer.decode(String.self, forKey: CodingKeys.copyright)
+    }
+}
