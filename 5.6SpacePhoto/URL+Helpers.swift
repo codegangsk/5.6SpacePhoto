947c5d983e670aed60044ffd0697f444d65cc159
codegangsk <kjhj0604@gmail.com>
2020-11-11 11:42:27 +0900

    declare function that gets queries

---
+++ 5.6SpacePhoto/URL+Helpers.swift
@@ -0,0 +1,16 @@
+//
+//  URL+Helpers.swift
+//  5.6SpacePhoto
+//
+//  Created by Sophie Kim on 2020/11/11.
+//
+
+import Foundation
+
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.map { URLQueryItem(name: $0.0, value: $0.1) }
+        return components?.url
+    }
+}
