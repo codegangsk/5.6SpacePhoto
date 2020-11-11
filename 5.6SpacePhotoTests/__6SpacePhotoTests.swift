6a4519d71d857806d547cddeec89d28ed527adb5
codegangsk <kjhj0604@gmail.com>
2020-11-11 10:46:52 +0900

    Initial Commit

---
+++ 5.6SpacePhotoTests/__6SpacePhotoTests.swift
@@ -0,0 +1,33 @@
+//
+//  __6SpacePhotoTests.swift
+//  5.6SpacePhotoTests
+//
+//  Created by Sophie Kim on 2020/11/11.
+//
+
+import XCTest
+@testable import __6SpacePhoto
+
+class __6SpacePhotoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
